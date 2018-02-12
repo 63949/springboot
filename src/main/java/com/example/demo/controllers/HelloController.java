62051dfce01fae7f6746727df5b6c8ffe1e1383e
cart88 <luopeizhong@gmail.com>
2018-02-12 21:50:28 +0800

    添加jrebel支持

--- src/main/java/com/example/demo/controllers/HelloController.java
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//http://localhost:8080/hello
+@RestController
 public class HelloController {
+    @RequestMapping("/hello")
+    public String sayHi(){
+        return  "Hi";
+    }
 }
