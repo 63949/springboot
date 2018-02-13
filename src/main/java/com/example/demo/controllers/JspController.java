4118d753af1a8ba2276841eed9a0e9055b7be5f4
cart88 <luopeizhong@gmail.com>
2018-02-13 17:30:46 +0800

    删除没用的JSPController方法

--- src/main/java/com/example/demo/controllers/JspController.java
+++ src/main/java/com/example/demo/controllers/JspController.java
@@ -3,22 +3,17 @@ package com.example.demo.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JspController {
-    @GetMapping(value = "/hellojsp0")
-    public String  init(){
-        return "index1";
-    }
-    @RequestMapping(value = "/hellojsp")
-    public String hello(){
-        return "index1";
-    }
 
-    @GetMapping
+
+    @GetMapping("/sss")
     String getView(Model model) {
 //        model.addAttribute("msg", "Hello there, This message has been injected from the controller method");
         return "helloworld";
     }
+
+
+
 }
